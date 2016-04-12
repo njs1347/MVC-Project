a80726d60fbcdc77e9daf50361e969cf606f00a1
Nyrih Smith <njs1347@rit.edu>
2016-04-12 14:27:57 -0400

    edit app

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ var RedisStore = require('connect-redis')(session);
 var url = require('url');
 var csrf = require('csurf');
 
-var dbURL = process.env.MONGOLAB_URI || "mongodb://localhost/Build-A-Bear";
+var dbURL = process.env.MONGODB_URI || "mongodb://localhost/Build-A-Bear";
 
 var db = mongoose.connect(dbURL, function(err){
    if(err){
